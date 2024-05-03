644409e452b2e06977e3b96eeb496e9027b3c2f6
Julien AGOSTINI <julien.agostini@businesspartner.roche.com>
2024-05-03 12:06:19 +0200

    Initial Commit

---
+++ MacAudioConverter/MacAudioConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAudioConverterApp.swift
+//  MacAudioConverter
+//
+//  Created by Julien AGOSTINI on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MacAudioConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
